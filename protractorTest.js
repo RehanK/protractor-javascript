aa0bac0976ebd687d2c388d22dba44803de74d4e
Rehan K <RehanK@users.noreply.github.com>
2016-06-27 15:57:29 -0500

    added expect statements to validate use cases

--- protractorTest.js
+++ protractorTest.js
@@ -1,22 +1,40 @@
 describe("Test Suite", function() {
 
-	  it ("should be on correct page", function() {
-	      browser.get("http://www.thetestroom.com/jswebapp/");
-	      expect(browser.getCurrentUrl()).toContain("jswebapp");
-	      expect(browser.getCurrentUrl()).toEqual("http://www.thetestroom.com/jswebapp/");
-	  });
+	it ("should be on correct page", function() {
+	    browser.get("http://www.thetestroom.com/jswebapp/");
+	    expect(browser.getCurrentUrl()).toContain("jswebapp");
+	    //expect(browser.getCurrentUrl()).toEqual("http://www.thetestroom.com/jswebapp/");
+	});
 
-	  it ("test2", function() {
-	      browser.get("http://www.thetestroom.com/jswebapp/");
-	      var textMessage = "Testing 123";
+	it ("should generate correct text", function() {
+	    browser.get("http://www.thetestroom.com/jswebapp/");
+	    var textMessage = "Testing 123";
 	      
-	      element(by.model("person.name")).sendKeys(textMessage);
-	      element(by.binding("person.name")).getText().then(function(text){
-	          expect(textMessage).toEqual("Testing 123");
+	    element(by.model("person.name")).sendKeys(textMessage);
+	    element(by.binding("person.name")).getText().then(function(text){
+	        expect(textMessage).toEqual("Testing 123");
+	    });
+	});
 
-	        
-	      });
+	it ("should check the correct number of dropdown options", function() {
+	    browser.get("http://www.thetestroom.com/jswebapp/");
+	    element.(by.buttonText("CONTINUE")).click();
+	    element(by.model("animal")).$("[value='1']").click();
+	    
+	    element.all(by.css(".ng-pristine option")).then(function(items){
+	    	expect(items.length).toBe(4);
+	    	expect(items[1]).getText()).toBe("George the Turtle");
+	    });
+	    
+	    element.(by.buttonText("CONTINUE")).click();
+	});
 
-    });
+	it ("should check user is on the thank you page", function() {
+	    browser.get("http://www.thetestroom.com/jswebapp/");
+	    element.(by.buttonText("CONTINUE")).click();
+	    element.(by.buttonText("CONTINUE")).click();
+	    expect(browser.getCurrectUrl()).toContain("confirm");
+	});
+
 
 });
