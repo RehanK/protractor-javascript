2f5487b57c7dda38ce86730a916fcd700c7d390e
Rehan K <RehanK@users.noreply.github.com>
2016-06-28 15:59:17 -0500

    added afterEach method

--- protractorTest.js
+++ protractorTest.js
@@ -2,7 +2,10 @@ describe("Test Suite", function() {
 	
 	beforeEach(function(){
 		browser.get("http://www.thetestroom.com/jswebapp/");	
-		
+	});
+	
+	afterEach(function(){
+		console.log("afterEach method executed")	
 	});
 
 	it ("should be on correct page", function() {
