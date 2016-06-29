ca8f450031c46d5428f9628e866080fd61fe7770
Rehan K <RehanK@users.noreply.github.com>
2016-06-29 15:26:40 -0500

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,34 @@
+describe("testing the js site", function(){
+
+  beforeEach(function() {
+    browser.get("http://www.thetestroom.com/jswebapp/");
+  });
+
+  it ("testing css expressions", function() {
+      
+      //css tag
+      element(by.css("input")).sendKeys("Text Test");
+      
+      //css class .
+      element(by.css(".ng-binding").getText().then(function(text) {
+        console.log(text);
+      });
+      
+      //css ID #
+      element(by.css("#continue_button")).click();
+      
+      //css chaining - chaining a tag#ID
+      element(by.css("button#continue_button"))
+      
+      //css child nodes - use spaces to locate each nodes
+      element(by.css("table td a")).getText().then(function(text) {
+        console.log(text);
+      });
+      
+      //css attribute
+      element(by.css("[id='tite']")).getText().then(function(text) {
+        console.log(text);
+      });
+  });
+
+});
