95d6cdeeb988298cfc4c828d5096332240b73da9
Rehan K <RehanK@users.noreply.github.com>
2016-07-05 02:55:42 -0500

    Create ExecutionTest.js

---
+++ page-object/ExecutionTest.js
@@ -0,0 +1,15 @@
+describe("Page Object Test", function() {
+	
+	beforeEach(function(){
+		browser.get("http://www.thetestroom.com/jswebapp/");	
+	});
+	
+	afterEach(function(){
+		console.log("afterEach method executed")	
+	});
+
+	it ("should be on correct page", function() {
+
+
+
+});
