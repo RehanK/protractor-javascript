bad0770fb169828875017a5bec988691c2a62912
Rehan K <RehanK@users.noreply.github.com>
2016-07-05 03:01:57 -0500

    Update ExecutionTest.js

--- page-object/ExecutionTest.js
+++ page-object/ExecutionTest.js
@@ -7,8 +7,16 @@ describe("Page Object Test", function() {
 	afterEach(function(){
 		console.log("afterEach method executed")	
 	});
+	
 
-	it ("should be on correct page", function() {
+	var home_page = require('home_page.js');
+
+	it ("should be able to enter text", function() {
+		home_page.enterFieldValue('pageOject Text');
+		var getHomePageText = home_page.getDynamicText();
+		expect(getHomePageText).toBe('pageOject Text');
+		home_page.clickContinue();
+	};
 
 
 
