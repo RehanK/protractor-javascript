94f57ec834fb343011dd6374874fd6e3023de9e9
Rehan K <RehanK@users.noreply.github.com>
2016-07-11 14:40:25 -0500

    Create nonAngularTest.js

---
+++ nonAngularTest.js
@@ -0,0 +1,11 @@
+describe("Test", function(){
+
+beforeEach(function() {
+      return browser.ignoreSynchronization = true;
+    });
+    
+    it("should do", function{
+    });
+
+});
+//http://ng-learn.org/2014/02/Protractor_Testing_With_Angular_And_Non_Angular_Sites/
