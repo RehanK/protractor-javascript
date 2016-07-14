8944b6769798e30e45874c5e6aeb7c81013f4a8c
Rehan K <RehanK@users.noreply.github.com>
2016-07-13 23:51:18 -0500

    Update nonAngularTest.js

--- nonAngularTest.js
+++ nonAngularTest.js
@@ -9,21 +9,32 @@ beforeEach(function() {
     	element(by.)
     });
 
-    searchText
-    element(by.id('search')).sendKeys("meguiars ultimate");
+    it("", function{
+      searchText
+      element(by.id('search')).sendKeys("meguiars ultimate");          
+    });
 
-    clickSearchButton
-    element(by.className('searchbar-submit js-searchbar-submit')).click();
 
-    grid view
-    element(by.className('grid-view wmicon wmicon-grid')).click();
+    it("", function{
+      clickSearchButton
+      element(by.className('searchbar-submit js-searchbar-submit')).click();          
+    });
 
-    selecting meguiars ultimate liquid wax
-    element(by.css('a[href*="/ip/Meguiar-s-Ultimate-Liquid-Wax/16550272"]')).click()
-
-    expect
-    element(by.partialLinkText('Meguiar-s-Ultimate-Liquid-Wax/16550272'));
-
+    it("", function{
+      grid view
+      element(by.className('grid-view wmicon wmicon-grid')).click();
+    });
+    
+    it("", function{
+      selecting meguiars ultimate liquid wax
+      element(by.css('a[href*="/ip/Meguiar-s-Ultimate-Liquid-Wax/16550272"]')).click()
+    });
+    
+    it("", function{
+      expect
+      element(by.partialLinkText('Meguiar-s-Ultimate-Liquid-Wax/16550272'));
+    });
+    
 
 
 });
