dbd7e29537e1a34a61a92388e30870566c7d49f5
Rehan K <RehanK@users.noreply.github.com>
2016-07-13 23:54:37 -0500

    Update nonAngularTest.js

--- nonAngularTest.js
+++ nonAngularTest.js
@@ -5,33 +5,24 @@ beforeEach(function() {
       browser.get('http://www.walmart.com/')
     });
     
-    it("should do", function{
-    	element(by.)
-    });
 
-    it("", function{
-      searchText
+    it("should search for item", function{
       element(by.id('search')).sendKeys("meguiars ultimate");          
     });
 
-
-    it("", function{
-      clickSearchButton
+    it("should click search button", function{
       element(by.className('searchbar-submit js-searchbar-submit')).click();          
     });
 
-    it("", function{
-      grid view
+    it("should switch to grid view", function{
       element(by.className('grid-view wmicon wmicon-grid')).click();
     });
     
-    it("", function{
-      selecting meguiars ultimate liquid wax
+    it("should select item", function{
       element(by.css('a[href*="/ip/Meguiar-s-Ultimate-Liquid-Wax/16550272"]')).click()
     });
     
-    it("", function{
-      expect
+    it("should expect", function{
       element(by.partialLinkText('Meguiar-s-Ultimate-Liquid-Wax/16550272'));
     });
     
