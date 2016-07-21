8fbb943937373f8a931cef270b03ca97c7e56c9f
Rehan K <RehanK@users.noreply.github.com>
2016-07-21 13:27:49 -0500

    Update Home.Page.js

--- e2e-testing/page-objects/Home.Page.js
+++ e2e-testing/page-objects/Home.Page.js
@@ -1,3 +1,11 @@
-var ("test", function(){
+  var HomePage = function() {
+    this.question = element(by.model('question.text'));
+    this.answer = element(by.binding('answer'));
+    this.button = element(by.className('question-button'));
 
-});
+    this.ask = function(question) {
+      this.question.sendKeys(question);
+      this.button.click();
+    };
+  };
+  module.exports = HomePage;
