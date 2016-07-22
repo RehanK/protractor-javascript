c4b6b418e25338344078ddcbbf8a589e0e68d688
Rehan K <RehanK@users.noreply.github.com>
2016-07-22 11:39:18 -0500

    Update home.pageObject.js

--- e2e-testing/page-objects/home.pageObject.js
+++ e2e-testing/page-objects/home.pageObject.js
@@ -1,11 +1,21 @@
   var HomePage = function() {
-    this.question = element(by.model('question.text'));
-    this.answer = element(by.binding('answer'));
-    this.button = element(by.className('question-button'));
-
-    this.ask = function(question) {
-      this.question.sendKeys(question);
-      this.button.click();
+    
+    this.get = function(){
+      browser.get('http://www.jetblue.com/')
     };
+    
+    this.email = element(by.id('email_field'));
+    this.password = element(by.id('password_field'));
+    this.signIn = element(by.id('signin_btn'));
+    
+    
+    //this.question = element(by.model('question.text'));
+    //this.answer = element(by.binding('answer'));
+    //this.button = element(by.className('question-button'));
+
+    //this.ask = function(question) {
+    //  this.question.sendKeys(question);
+    //  this.button.click();
+    //};
   };
   module.exports = HomePage;
