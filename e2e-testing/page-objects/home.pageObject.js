b89dccdec612762fc6fe2670a836b2f935f2d416
Rehan K <RehanK@users.noreply.github.com>
2016-07-30 21:14:59 -0500

    added login functions

--- e2e-testing/page-objects/home.pageObject.js
+++ e2e-testing/page-objects/home.pageObject.js
@@ -4,10 +4,19 @@
       browser.get('http://www.jetblue.com/')
     };
     
-    this.email = element(by.id('email_field'));
-    this.password = element(by.id('password_field'));
-    this.signIn = element(by.id('signin_btn'));
+    var Login = function(){
+      this.email = element(by.id('email_field'));
+      this.password = element(by.id('password_field'));
+      this.signIn = element(by.id('signin_btn'));
+      this.rememberEmail = element(by.css('remembermeCheckbox'));
+      this.forgotPassword  element(by.css('a[href="https://trueblue.jetblue.com/web/trueblue/forgot-password"]'));
+    };
     
+    var Flights = function(){
+      
+      
+    
+    };
     
     //this.question = element(by.model('question.text'));
     //this.answer = element(by.binding('answer'));
